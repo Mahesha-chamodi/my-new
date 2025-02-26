782395d8293d15aca01f2cc2633072169840a193
Mahesha Chamodi <maheshachamodi632@gmail.com>
2025-02-26 12:13:43 +0530

    first upload

---
+++ component/header.php
@@ -0,0 +1,13 @@
+<section class="header">
+    <a href="Home.php" class="logo">Wedding.</a>
+
+    <nav class="navbar">
+        <a href="Home.php">Home</a>
+        <a href="about.php">about</a>
+        <a href="Portfolio.php">Portfolio</a>
+        <a href="Pricing.php">Pricing</a>
+        <a href="Contact.php">Contact</a>
+
+</nav>
+
+</section>
